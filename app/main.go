80614a3dd207f0c12c4794bfdf7177d786927b04
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-08-09 15:35:53 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	ln, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		panic(err)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go handleConnection(conn)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+		//conn.Write([]byte("-Error invalid command: '" + text + "'\r\n"))
+	}
 }
