5e856270ce9b44849d9dc50b0991373261504971
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-08-09 15:43:03 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -31,6 +31,11 @@ func handleConnection(conn net.Conn) {
 		text := scanner.Text()
 		if strings.TrimSpace(text) == "PING" {
 			conn.Write([]byte("+PONG\r\n"))
+		}else{
+			inpString := strings.Split(text, " ")
+			if len(inpString) > 1 && inpString[0] == "ECHO" {
+				conn.Write([]byte(fmt.Sprintf("%s\r\n",inpString[1])))
+			}
 		}
 		//conn.Write([]byte("-Error invalid command: '" + text + "'\r\n"))
 	}
